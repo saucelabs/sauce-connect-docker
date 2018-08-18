f1ba6ae22a8345b7e3ce7293cae87ad1fe732ed0
Zwane Mwaikambo <zwane.mwaikambo@saucelabs.com>
2018-08-18 13:10:46 -0700

    - Accept commandline arguments - Use host networking

---
+++ entrypoint.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+${SERVICE_HOME}/sc-${SERVICE_VERSION}-linux/bin/sc --logfile=${SC_LOG_FILE} $@
--- src/sc.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-${SRV_HOME}/sc-${SERVICE_VERSION}-linux/bin/sc --logfile=${SC_LOG_FILE} -i ${HOSTNAME} --extra-info=${SC_EXTRA_INFO}
