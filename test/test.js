b2f0c552da051b0caba217cb8ab6fbf24e7601e6
Dan Slov <dan.slov@saucelabs.com>
2022-04-07 17:48:19 -0700

    Adding wait-on to the tests

--- test/test.js
+++ test/test.js
@@ -1,17 +1,65 @@
 const assert = require('assert')
 const { remote } = require('webdriverio')
 const request = require('request');
+const waitOn = require('wait-on');
+const fs = require('fs');
+const path = require('path');
+const readinessURL = 'http://localhost:8032/readiness'
+const statusURL = 'http://localhost:8032/api/v1/status'
+const readyFile = '/tmp/sc.ready'
+var opts = {
+  resources: [readinessURL],
+  delay: 50000, // initial delay in ms
+  interval: 500, // poll interval in ms
+  simultaneous: 1, // limit to 1 connection per resource at a time
+  timeout: 30000, // timeout in ms
+  tcpTimeout: 1000, // tcp timeout in ms, default 300ms
+  validateStatus: function (status) {
+    return status >= 200 && status < 300;
+  }
+}
 
-;(async () => {
-    if (process.env.DIST_TAG.includes('4.8.')) {
+function checkExistsWithTimeout(filePath, timeout) {
+    return new Promise(function (resolve, reject) {
+
+        var timer = setTimeout(function () {
+            watcher.close()
+            reject(new Error('File did not exists and was not created during the timeout.'))
+        }, timeout)
+
+        fs.access(filePath, fs.constants.R_OK, function (err) {
+            if (!err) {
+                clearTimeout(timer)
+                watcher.close()
+                resolve()
+            }
+        })
+
+        var dir = path.dirname(filePath)
+        var basename = path.basename(filePath)
+        var watcher = fs.watch(dir, function (eventType, filename) {
+            if (eventType === 'rename' && filename === basename) {
+                clearTimeout(timer)
+                watcher.close()
+                console.log('File exists', filePath)
+                resolve()
+            }
+        })
+    })
+}
+
+(async () => {
+
+    if (process.env.DIST_TAG && process.env.DIST_TAG.includes('4.8.')) {
+        await waitOn(opts);
         console.log("checking status for SC version", process.env.DIST_TAG);
-        request({url: 'http://localhost:8032/api/v1/status', json: true }, (err, res, body) => {
+        request({url: statusURL, json: true }, (err, res, body) => {
         assert.equal(err, null)
         console.log("Sauce Connect status %j", body);
         assert.ok(body.tunnelStatus.includes('connected'), JSON.stringify(body))
         })
     } else {
-        console.log("not checking status for SC version", process.env.DIST_TAG);
+        await checkExistsWithTimeout(readyFile, opts.timeout)
     }
     const browser = await remote({
         user: process.env.SAUCE_USERNAME,
@@ -37,6 +85,7 @@ const request = require('request');
     assert.equal(text, 'Sauce Connect Success!')
     
     console.log('Success!')
-})().catch(() => {
+})().catch((err) => {
+    console.error(err)
     process.exit(1)
 })
--- test/wait-for-sc.sh
+++
@@ -1,6 +0,0 @@
-until [ -f /tmp/sc.ready ]
-do
-     sleep 1
-done
-echo "SC ready"
-exit
